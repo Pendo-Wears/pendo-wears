0004e9a98b67b3613236cc75092e1d85f0c96081
Alongedaniel <alongedaniel41@gmail.com>
2026-01-06 00:17:04 +0100

    x

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,18 +1,12 @@
-import { productsEndpoint } from "@/src/lib/endpoints";
-import React from "react";
+import React from 'react'
 
-const Products = async () => {
-  const products = await productsEndpoint.getProducts();
+const page = () => {
   return (
     <div>
-      <h1>Products</h1>
-      <ul>
-        {products.map((product: any) => (
-          <li key={product.id}>{product.name}</li>
-        ))}
-      </ul>
+      
     </div>
-  );
-};
+  )
+}
 
-export default Products;
+export default page
+
