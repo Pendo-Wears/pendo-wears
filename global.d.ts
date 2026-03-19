144137413615841c4c6c03dcd775e5d57ec1f938
Alongedaniel <alongedaniel41@gmail.com>
2026-03-19 16:08:06 +0100

    typescript error fix

--- global.d.ts
+++ global.d.ts
@@ -1,7 +1,7 @@
 export declare global {
   interface Window {
     __gaLoaded?: boolean;
-    dataLayer: unknown[];
-    gtag: (...args: unknown[]) => void;
+    dataLayer: any[];
+    gtag: (...args: any[]) => void;
   }
 }
